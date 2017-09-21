0e79d4a60ff9cccf3fa98b19de97723459da7b99
Aleksandr Drozd <alexander.drozd@gmail.com>
2017-09-21 17:51:15 +0900

    cleanup

--- dagen/image/image.py
+++ dagen/image/image.py
@@ -1,5 +1,4 @@
 import numpy as np
-import random
 import PIL
 import PIL.Image
 from contextfree import contextfree as cf
@@ -46,12 +45,6 @@ def get_counting_sample():
 
 
 def gen_item(label, test=False):
-    # if test:
-    #    xc_random = int(random.random() * (dim_image // 2 - radius * 2 - 1) + radius + dim_image // 2 + 1)
-    # else:
-    #    xc_random = int(random.random() * (dim_image // 2 - radius * 2 - 1) + radius)
-    # yc_random = int(random.random() * (dim_image - radius * 2) + radius)
-    # r = int(random.random() * (min_radius) + radius - min_radius)
     if label == 0:
         a = get_circle()
     else:
@@ -86,7 +79,7 @@ def main():
     X_train = np.array([gen_item(i % 2) for i in range(cnt_samples)])
     Y_train = np.array([i % 2 for i in range(cnt_samples)], dtype=np.int32)
     print(X_train.shape, Y_train.shape)
-    im = merge_sample(X_train, Y_train)
+    im = merge_samples(X_train, Y_train)
     im.save("/tmp/test.png")
 
 if __name__ == "__main__":
