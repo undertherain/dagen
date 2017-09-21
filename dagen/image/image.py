bbbb2e55a68fdbc1196b509aa7cac9a71813bcab
Aleksandr Drozd <alexander.drozd@gmail.com>
2017-09-21 16:47:36 +0900

    generating dataset for counting

--- dagen/image/image.py
+++ dagen/image/image.py
@@ -11,44 +11,75 @@ dim_image = 64
 
 def get_box():
     cf.init(canvas_size=(dim_image, dim_image), face_color="#FFFFFF")
-    with cf.translate(cf.rnd(1), cf.rnd(1)):
-        cf.box(cf.rnd(0.5) + 0.75)
+    with cf.translate(cf.rnd(0.5), cf.rnd(0.5)):
+        cf.box(cf.rnd(0.5) + 0.4)
     a = cf.get_npimage()
     return a[:, :, 0]
 
 
 def get_circle():
     cf.init(canvas_size=(dim_image, dim_image), face_color="#FFFFFF")
-    with cf.translate(cf.rnd(1), cf.rnd(1)):
-        cf.circle(0.5 + cf.rnd(0.5))
+    with cf.translate(cf.rnd(0.5), cf.rnd(0.5)):
+        cf.circle(0.25 + cf.rnd(0.2))
     a = cf.get_npimage()
     return a[:, :, 0]
 
 
-def gen_item(cl, test=False):
-    a = np.zeros([dim_image, dim_image], dtype=np.float32)
-    radius = 12
-    min_radius = 6
-    if test:
-        xc_random = int(random.random() * (dim_image // 2 - radius * 2 - 1) + radius + dim_image // 2 + 1)
-    else:
-        xc_random = int(random.random() * (dim_image // 2 - radius * 2 - 1) + radius)
-    yc_random = int(random.random() * (dim_image - radius * 2) + radius)
-    r = int(random.random() * (min_radius) + radius-min_radius)
-    if cl == 0:
+def get_counting_sample():
+    cf.init(canvas_size=(dim_image, dim_image), face_color="#FFFFFF")
+    cnt_circles = 0
+    cnt_boxes = 0
+    cnt_items = 3
+    with cf.scale(1 / (cnt_items + 1)):
+        with cf.translate(-cnt_items / 2, -cnt_items / 2):
+            for y in range(cnt_items):
+                with cf.translate(0, y * 1.1):
+                    for x in range(cnt_items):
+                        with cf.translate(x * 1.1, 0):
+                            if cf.coinflip(2):
+                                if cf.coinflip(2):
+                                    cf.circle(0.4 + cf.rnd(0.2))
+                                    cnt_circles += 1
+                                else:
+                                    cf.box(0.85 + cf.rnd(0.2))
+                                    cnt_boxes += 1
+    a = cf.get_npimage()
+    return a[:, :, 0], cnt_circles, cnt_boxes
+
+
+def gen_item(label, test=False):
+    # if test:
+    #    xc_random = int(random.random() * (dim_image // 2 - radius * 2 - 1) + radius + dim_image // 2 + 1)
+    # else:
+    #    xc_random = int(random.random() * (dim_image // 2 - radius * 2 - 1) + radius)
+    # yc_random = int(random.random() * (dim_image - radius * 2) + radius)
+    # r = int(random.random() * (min_radius) + radius - min_radius)
+    if label == 0:
         a = get_circle()
     else:
         a = get_box()
     return a
 
 
-def merge_sample(X, Y, cnt_sampes=10):
+def merge_samples(X, Y, cnt_sampes=10):
     bar = np.ones([X.shape[1], 2])
     im_ar = np.hstack([np.hstack([X[i], bar]) for i in range(cnt_sampes)])
     im = PIL.Image.fromarray(im_ar * 255)
     if im.mode != 'RGB':
         im = im.convert('RGB')
     return im
+
+
+def get_ds_counting():
+    data = []
+    cnts = []
+    for i in range(10):
+        a, c, b = get_counting_sample()
+        data.append(a)
+        cnts.append([c, b])
+    X = np.array(data)
+    Y = np.array(cnts, dtype=np.int32)
+    return X, Y
 
 
 def main():
--- test/test_image.py
+++ test/test_image.py
@@ -1,16 +1,23 @@
 
 import unittest
 import numpy as np
-from dagen.image.image import gen_item, merge_sample
+from dagen.image.image import gen_item, merge_samples, get_ds_counting
 
 
 class Tests(unittest.TestCase):
 
-    def test_file_iter(self):
+    def test_simple(self):
         print("generating sample output")
         cnt_samples = 10
         X_train = np.array([gen_item(i % 2) for i in range(cnt_samples)])
         Y_train = np.array([i % 2 for i in range(cnt_samples)], dtype=np.int32)
         print(X_train.shape, Y_train.shape)
-        im = merge_sample(X_train, Y_train)
+        im = merge_samples(X_train, Y_train)
         im.save("/tmp/test.png")
+
+    def test_counting(self):
+        print("generating sample output")
+        X_train, Y_train = get_ds_counting()
+        print(X_train.shape, Y_train.shape)
+        im = merge_samples(X_train, Y_train)
+        im.save("/tmp/counting.png")
